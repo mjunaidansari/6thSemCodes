ecc6a61df64780a0b3b41ff62ddd0299de351317
Junaid Ansari <junaidshahid.ansari@gmail.com>
2024-03-14 20:24:23 +0530

    added simple c code for dead code elimination which takes multiple lines as input and displays code before return statement

---
+++ System Programming and Compiler Construction/deadCode.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+void main() {
+
+	char code[15][50];
+	int n = 0;
+	int i;
+
+	printf("\nEnter your C code (enter empty line to finish):\n\n");
+	while(n < 15 && fgets(code[n], sizeof(code[0]), stdin)){
+		if(code[n][0] == '\n')
+			break;
+		n++;
+	}
+
+	printf("Code after Dead Code Elimination: \n\n");
+	for(i=0; i<n; i++){
+		printf("%s", code[i]);
+		if(strcmp(strtok(code[i], " "), "\treturn") == 0)
+			printf("\n");
+			break;
+	}
+
+}
